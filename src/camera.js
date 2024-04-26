c64cadf77aedd02054b52996aebfd3ee96e388a4
Kim Jeong-won <threedalpeng@gmail.com>
2024-04-26 15:50:06 +0900

    feat: change camera position

--- src/camera.js
+++ src/camera.js
@@ -6,17 +6,21 @@ import { canvas1, canvas2 } from "./dom.js";
 export const createCameras = async (scene) => {
   const camera1 = new BABYLON.UniversalCamera(
     "camera1",
-    new BABYLON.Vector3(0, 5, -20),
+    new BABYLON.Vector3(0, 8, -20),
     scene
   );
-  camera1.setTarget(new BABYLON.Vector3(0, 5, 0));
+  camera1.setTarget(new BABYLON.Vector3(0, 5, -10));
+  camera1.fov = 0.93;
+  camera1.fovMode = BABYLON.Camera.FOVMODE_HORIZONTAL_FIXED;
 
   const camera2 = new BABYLON.UniversalCamera(
     "camera2",
-    new BABYLON.Vector3(0, 5, 20),
+    new BABYLON.Vector3(0, 8, 20),
     scene
   );
-  camera2.setTarget(new BABYLON.Vector3(0, 5, 0));
+  camera2.setTarget(new BABYLON.Vector3(0, 5, 10));
+  camera2.fov = 0.93;
+  camera2.fovMode = BABYLON.Camera.FOVMODE_HORIZONTAL_FIXED;
 
   const engine = scene.getEngine();
   engine.registerView(canvas1, camera1);
@@ -25,4 +29,17 @@ export const createCameras = async (scene) => {
   scene.detachControl();
   engine.inputElement = canvas1;
   scene.attachControl();
+
+  scene.onBeforeCameraRenderObservable.add(() => {
+    const { width1, height1 } = canvas1.getBoundingClientRect();
+    camera1.fovMode =
+      width1 > height1
+        ? BABYLON.Camera.FOVMODE_HORIZONTAL_FIXED
+        : BABYLON.Camera.FOVMODE_VERTICLE_FIXED;
+    const { width2, height2 } = canvas2.getBoundingClientRect();
+    camera1.fovMode =
+      width2 > height2
+        ? BABYLON.Camera.FOVMODE_HORIZONTAL_FIXED
+        : BABYLON.Camera.FOVMODE_VERTICLE_FIXED;
+  });
 };
